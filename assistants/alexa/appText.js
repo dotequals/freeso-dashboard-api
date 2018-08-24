2503858bc7251f884fbccfaf5fe374dc5224e04a
dotequals <strdotequals@gmail.com>
2018-08-23 17:40:05 -0700

    Updating text with new skills

--- assistants/alexa/appText.js
+++ assistants/alexa/appText.js
@@ -1,8 +1,10 @@
+const helpText = 'You can ask me: "How many sims are online?", "How many lots are online?" and "Where is everybody?"';
+
 const appText = {
   errorText: 'Sorry, I can\'t understand the command. Please say it again.',
-  helpText: 'You can ask me how many sims are online',
+  helpText,
   skillName: 'FreeSO Dashboard',
-  welcomeText: 'Welcome to FreeSO Dashboard. You can ask me how many sims are online.',
+  welcomeText: `Welcome to FreeSO Dashboard. ${helpText}`,
 };
 
 module.exports = appText;
